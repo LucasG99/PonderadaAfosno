adfcb7c8acece6e3930e4cc7689964efc4fa4421
LucasG99 <lucas.galvao@sou.inteli.edu.br>
2025-03-27 19:02:01 -0300

    Corrige configuração de arquivos estáticos e remove dependências não existentes

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -17,11 +17,9 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
